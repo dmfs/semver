afad240ee831f0cd3a79ec2b7dadfe4a8cf8bb21
Marten Gajda <marten@dmfs.org>
2022-01-07 22:48:53 +0100

    Improve NextPreRelease Test coverage

--- src/test/java/org/dmfs/semver/NextPreReleaseTest.java
+++ src/test/java/org/dmfs/semver/NextPreReleaseTest.java
@@ -11,6 +11,57 @@ import static org.hamcrest.Matchers.is;
 class NextPreReleaseTest
 {
     @Test
+    void testAlphaVersion()
+    {
+        assertThat(new NextPreRelease(new PreRelease(new Release(1, 0, 0), "alpha")),
+            is(preRelease(version(1, 0, 0), "alpha.1")));
+
+        assertThat(new NextPreRelease(new PreRelease(new Release(1, 0, 0), "alpha.1")),
+            is(preRelease(version(1, 0, 0), "alpha.2")));
+
+        assertThat(new NextPreRelease(new PreRelease(new Release(1, 0, 0), "beta")),
+            is(preRelease(version(1, 0, 0), "beta.1")));
+
+        assertThat(new NextPreRelease(new PreRelease(new Release(1, 0, 0), "beta.1")),
+            is(preRelease(version(1, 0, 0), "beta.2")));
+
+        assertThat(new NextPreRelease(new Release(1, 0, 0)),
+            is(preRelease(version(1, 1, 0), "alpha")));
+
+        assertThat(new NextPreRelease(new PreRelease(new Release(1, 2, 0), "alpha")),
+            is(preRelease(version(1, 2, 0), "alpha.1")));
+
+        assertThat(new NextPreRelease(new PreRelease(new Release(1, 2, 0), "alpha.1")),
+            is(preRelease(version(1, 2, 0), "alpha.2")));
+
+        assertThat(new NextPreRelease(new PreRelease(new Release(1, 2, 0), "beta")),
+            is(preRelease(version(1, 2, 0), "beta.1")));
+
+        assertThat(new NextPreRelease(new PreRelease(new Release(1, 2, 0), "beta.1")),
+            is(preRelease(version(1, 2, 0), "beta.2")));
+
+        assertThat(new NextPreRelease(new Release(1, 2, 0)),
+            is(preRelease(version(1, 3, 0), "alpha")));
+
+        assertThat(new NextPreRelease(new PreRelease(new Release(1, 2, 3), "alpha")),
+            is(preRelease(version(1, 2, 3), "alpha.1")));
+
+        assertThat(new NextPreRelease(new PreRelease(new Release(1, 2, 3), "alpha.1")),
+            is(preRelease(version(1, 2, 3), "alpha.2")));
+
+        assertThat(new NextPreRelease(new PreRelease(new Release(1, 2, 3), "beta")),
+            is(preRelease(version(1, 2, 3), "beta.1")));
+
+        assertThat(new NextPreRelease(new PreRelease(new Release(1, 2, 3), "beta.1")),
+            is(preRelease(version(1, 2, 3), "beta.2")));
+
+        assertThat(new NextPreRelease(new Release(1, 2, 3), "beta"),
+            is(preRelease(version(1, 3, 0), "alpha")));
+
+    }
+
+
+    @Test
     void test()
     {
         assertThat(new NextPreRelease(new PreRelease(new Release(1, 0, 0), "alpha"), "beta"),
